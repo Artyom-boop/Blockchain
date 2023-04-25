1e813008e2272449dbb657d9e3de3ce5f29c8d4f
Artyom-boop <artyom.sidor2001@gmail.com>
2023-04-25 14:11:22 +0300

    Create Server

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,10 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Node node = new Node(Integer.parseInt(args[0]));
+        node.start();
+        while (true) {
+            node.createBlock();
+        }
     }
 }
